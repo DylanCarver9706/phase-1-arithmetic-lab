2b5d826642a62aef2bd39587c85319f0bf8d0da6
Dylan <dylancarver14@gmail.com>
2022-07-19 12:55:50 -0500

    commit tree 87777838b5b5c6b6a3fbc3a430ce6dc6d8fb8a7e

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
+   const num1 = 31
+   const num2 = 2
+   const multiply = function (num1, num2) {
+    return num1 * num2
+  }
 
-// Write your code here
+  function random() {
+    return Math.floor(Math.random()) + 1
+  }
+
+//   function random(min, max) { 
+//     min = Math.ceil(min);
+//     max = Math.floor(max);
+//     return Math.floor(Math.random() * (max - min + 1)) + min
+
+
+  //   function random() {
+//     min = Math.floor(Math.random()) + 2;
+//     max = Math.ceil(Math.infinite);
+//     return Math.random(max) + 2
+//}
